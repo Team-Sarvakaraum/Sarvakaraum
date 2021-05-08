3827dc16d0590f58dcaced718086bff2ffb5ca4e
Aayushjoshi28 <aayushj8355@gmail.com>
2021-05-09 02:52:16 +0530

    reformated all the code

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # www.sarvakaraum.com
+
 Website 2021
 https://main.d38vu5k4ddi9vg.amplifyapp.com/
--- assets/js/autotypejs/scribbler.js
+++ assets/js/autotypejs/scribbler.js
@@ -1,18 +1,18 @@
 // utilities
 var get = function (selector, scope) {
-  scope = scope ? scope : document;
-  return scope.querySelector(selector);
+    scope = scope ? scope : document;
+    return scope.querySelector(selector);
 };
 
 var getAll = function (selector, scope) {
-  scope = scope ? scope : document;
-  return scope.querySelectorAll(selector);
+    scope = scope ? scope : document;
+    return scope.querySelectorAll(selector);
 };
 
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
-  var i = 0;
-  var txt = `#शामिल <स्टडीओ.ह >
+    var i = 0;
+    var txt = `#शामिल <स्टडीओ.ह >
             शून्य swap(पूर्णांक , पूर्णांक );
             पूर्णांक मुख्य ()
             {
@@ -31,51 +31,51 @@ if (document.getElementsByClassName('demo').length > 0) {
                 ` + Array(5).fill('\xa0').join('') + `*b = *a;
                 ` + Array(5).fill('\xa0').join('') + `*a = temp;
             }`;
-  var speed = 1;
+    var speed = 1;
 
-  function typeItOut () {
-    if (i < txt.length) {
-      document.getElementsByClassName('demo')[0].innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeItOut, speed);
+    function typeItOut() {
+        if (i < txt.length) {
+            document.getElementsByClassName('demo')[0].innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeItOut, speed);
+        }
     }
-  }
 
-  setTimeout(typeItOut, 1800);
+    setTimeout(typeItOut, 1800);
 }
 
 // toggle tabs on codeblock
-window.addEventListener("load", function() {
-  // get all tab_containers in the document
-  var tabContainers = getAll(".tab__container");
+window.addEventListener("load", function () {
+    // get all tab_containers in the document
+    var tabContainers = getAll(".tab__container");
 
-  // bind click event to each tab container
-  for (var i = 0; i < tabContainers.length; i++) {
-    get('.tab__menu', tabContainers[i]).addEventListener("click", tabClick);
-  }
-
-  // each click event is scoped to the tab_container
-  function tabClick (event) {
-    var scope = event.currentTarget.parentNode;
-    var clickedTab = event.target;
-    var tabs = getAll('.tab', scope);
-    var panes = getAll('.tab__pane', scope);
-    var activePane = get(`.${clickedTab.getAttribute('data-tab')}`, scope);
-
-    // remove all active tab classes
-    for (var i = 0; i < tabs.length; i++) {
-      tabs[i].classList.remove('active');
+    // bind click event to each tab container
+    for (var i = 0; i < tabContainers.length; i++) {
+        get('.tab__menu', tabContainers[i]).addEventListener("click", tabClick);
     }
 
-    // remove all active pane classes
-    for (var i = 0; i < panes.length; i++) {
-      panes[i].classList.remove('active');
-    }
+    // each click event is scoped to the tab_container
+    function tabClick(event) {
+        var scope = event.currentTarget.parentNode;
+        var clickedTab = event.target;
+        var tabs = getAll('.tab', scope);
+        var panes = getAll('.tab__pane', scope);
+        var activePane = get(`.${clickedTab.getAttribute('data-tab')}`, scope);
 
-    // apply active classes on desired tab and pane
-    clickedTab.classList.add('active');
-    activePane.classList.add('active');
-  }
+        // remove all active tab classes
+        for (var i = 0; i < tabs.length; i++) {
+            tabs[i].classList.remove('active');
+        }
+
+        // remove all active pane classes
+        for (var i = 0; i < panes.length; i++) {
+            panes[i].classList.remove('active');
+        }
+
+        // apply active classes on desired tab and pane
+        clickedTab.classList.add('active');
+        activePane.classList.add('active');
+    }
 });
 
 //in page scrolling for documentaiton page
@@ -83,58 +83,59 @@ var btns = getAll('.js-btn');
 var sections = getAll('.js-section');
 
 function setActiveLink(event) {
-  // remove all active tab classes
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].classList.remove('selected');
-  }
+    // remove all active tab classes
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].classList.remove('selected');
+    }
 
-  event.target.classList.add('selected');
+    event.target.classList.add('selected');
 }
 
 function smoothScrollTo(i, event) {
-  var element = sections[i];
-  setActiveLink(event);
+    var element = sections[i];
+    setActiveLink(event);
 
-  window.scrollTo({
-    'behavior': 'smooth',
-    'top': element.offsetTop - 20,
-    'left': 0
-  });
+    window.scrollTo({
+        'behavior': 'smooth',
+        'top': element.offsetTop - 20,
+        'left': 0
+    });
 }
 
 if (btns.length && sections.length > 0) {
-  for (var i = 0; i<btns.length; i++) {
-    btns[i].addEventListener('click', smoothScrollTo.bind(this,i));
-  }
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener('click', smoothScrollTo.bind(this, i));
+    }
 }
 
 // fix menu to page-top once user starts scrolling
 window.addEventListener('scroll', function () {
-  var docNav = get('.doc__nav > ul');
+    var docNav = get('.doc__nav > ul');
 
-  if( docNav) {
-    if (window.pageYOffset > 63) {
-      docNav.classList.add('fixed');
-    } else {
-      docNav.classList.remove('fixed');
+    if (docNav) {
+        if (window.pageYOffset > 63) {
+            docNav.classList.add('fixed');
+        } else {
+            docNav.classList.remove('fixed');
+        }
     }
-  }
 });
 
 // responsive navigation
 var topNav = get('.menu');
 var icon = get('.toggle');
 
-window.addEventListener('load', function(){
-  function showNav() {
-    if (topNav.className === 'menu') {
-      topNav.className += ' responsive';
-      icon.className += ' open';
-    } else {
-      topNav.className = 'menu';
-      icon.classList.remove('open');
+window.addEventListener('load', function () {
+    function showNav() {
+        if (topNav.className === 'menu') {
+            topNav.className += ' responsive';
+            icon.className += ' open';
+        } else {
+            topNav.className = 'menu';
+            icon.classList.remove('open');
+        }
     }
-  }
-  icon.addEventListener('click', showNav);
+
+    icon.addEventListener('click', showNav);
 });
 
